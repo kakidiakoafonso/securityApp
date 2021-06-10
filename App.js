5fde16cd019bc4f783aa204ab544a46fa21b94a4
Kakidiako Afonso <kakidiakoafonso@gmail.com>
2021-06-10 12:36:27 +0100

    Falta

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react'
 import Home from './src/Home';
+import Splash from './src/Splash';
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:true}}>
+      <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown:true}}>
+        <Stack.Screen name="Splash" component={Splash} options={{headerShown:false}}/>
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/Splash.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Icon from './svg/20203-security.json'
+
+export default function Splash() {
+    return (
+        <View style={{flex:1}}>
+            <Text>AAA</Text>
+        </View>
+    )
+}
